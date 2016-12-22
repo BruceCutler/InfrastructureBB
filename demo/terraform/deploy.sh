0ef4498a7f5bdbb42328c3f6fcd6aeb0b8d3b749
BruceCutler <bicutler10@gmail.com>
2016-12-22 08:56:54 -0500

    TerraformDemo - Adding public and private subnets

--- demo/terraform/deploy.sh
+++ demo/terraform/deploy.sh
@@ -4,18 +4,22 @@ set -e
 
 geo=$GEO
 stack=$STACK
-action=$ACTION
+target=$TARGET
+action=$ACT
 
-while getopts ':g:s:a:' opt; do
+while getopts ':g:t:s:a:' opt; do
   case $opt in
     g)
       geo="$OPTARG"
+      ;;
+    t)
+      target="$OPTARG"
       ;;
     s)
       stack="$OPTARG"
       ;;
     a)
-      action="$OPTARG"
+      ACTION="$OPTARG"
       ;;
   esac
 done
@@ -23,16 +27,16 @@ done
 # Set up the AWS Region basd on the geo
 case $geo in
   us)
-    region="us-east-2"
+    region="us-east-1"
     ;;
   uk)
-    region="eu-west-1"
+    region="eu-central-1"
     ;;
 esac
 
 # Set up variables for Terraform remote config
 bucket="slalom-$geo"
-BUCKET_KEY="state/$stack.tfstate"
+BUCKET_KEY="state/$target-$stack.tfstate"
 
 # Check to make sure the stack exists
 if [[ ! -d $SOURCE_DIR/stacks/$stack ]]; then
@@ -49,12 +53,12 @@ VAR_DIR=$SOURCE_DIR/vars
 VARS_FILE_LIST=""
 
 # Read appropriate variable files from default to most granular
-for var_file in "default" "$stack" "$geo/default" "$geo/stack"; do
+for var_file in "default" "$stack" "$target" "$geo/default" "$geo/$stack" "$geo/$target/default" "$geo/$target/$stack" ; do
   [[ -f "$VAR_DIR/$var_file.tfvars" ]] && echo "Found: [$var_file.tfvars]" && VARS_FILE_LIST="$VARS_FILE_LIST -var-file $VAR_DIR/$var_file.tfvars"
 done
 
 # Set up TMP dir to run Terraform commands in
-TMP_DIR=$SOURCE_DIR/../tmp/$geo-$stack-$(date "+%Y-%m-%d-%H%M%S")
+TMP_DIR=$SOURCE_DIR/../tmp/$target-$geo-$stack-$(date "+%Y-%m-%d-%H%M%S")
 mkdir -p $TMP_DIR
 
 # Initialize terraform backend
@@ -66,14 +70,16 @@ terraform init $SOURCE_DIR
 # Generate input variable file and add to var file list
 cat > $TMP_DIR/vars/inputVars.tfvars << EOF
 geo="$geo"
+target="$target"
 stack="$stack"
+region="$region"
 EOF
 
 echo
 cat $TMP_DIR/vars/inputVars.tfvars
 echo
 
-VARS_FILE_FLAG="-var-file $TMP_DIR/vars/inputVars.tfvars $VARS_FILE_LIST"
+VARS_FILE_LIST="-var-file $TMP_DIR/vars/inputVars.tfvars $VARS_FILE_LIST"
 
 
 # Set up the Terraform remote state in S3
