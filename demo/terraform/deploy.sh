1645617f72f0f1e06488a41a3fda3c106d696ea3
BruceCutler <bicutler10@gmail.com>
2017-02-24 16:18:22 -0500

    TerraformDemo - Updates to deploy wrapper

--- demo/terraform/deploy.sh
+++ demo/terraform/deploy.sh
@@ -3,4 +3,12 @@
 geo=$1
 target=$2
 
-./tf_deploy -g $geo -t $target -s networking -a plan
+./tf_deploy -g $geo -t $target -s networking -a apply
+
+sleep 10
+
+./tf_deploy -g $geo -t $target -s data -a apply
+
+sleep 10
+
+./tf_deploy -g $geo -t $target -s application -a apply
